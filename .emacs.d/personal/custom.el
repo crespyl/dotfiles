3c4a906ca9832bf1acdd6ce57e9619ec93bcb9f6
Peter Jacobs <peter.jacobs@ambassador-enterprises.com>
2014-07-25 07:49:44 -0400

    Added custom.el

---
+++ .emacs.d/personal/custom.el
@@ -0,0 +1,46 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
+ '(blink-matching-paren nil)
+ '(fci-rule-color "#383838")
+ '(global-hl-line-mode t)
+ '(helm-match-plugin-mode t nil (helm-match-plugin))
+ '(ido-enable-flex-matching t)
+ '(indent-tabs-mode t)
+ '(prelude-whitespace nil)
+ '(smartrep-mode-line-active-bg "#383838")
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3")
+ '(whitespace-line-column 100))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-group-tag ((t (:inherit variable-pitch :foreground "color-39" :weight bold :height 1.2))))
+ '(custom-variable-tag ((t (:foreground "color-25" :weight bold))))
+ '(flyspell-duplicate ((t (:inherit warning :underline t))))
+ '(font-lock-comment-face ((t (:foreground "color-243"))))
+ '(font-lock-function-name-face ((t (:foreground "brightblue"))))
+ '(font-lock-string-face ((t (:foreground "color-70"))))
+ '(helm-selection ((t (:background "color-23" :underline t))))
+ '(helm-visible-mark ((t (:background "color-75"))))
+ '(highlight ((t (:background "color-234"))))
+ '(isearch ((t (:background "color-34" :foreground "brightwhite"))))
+ '(lazy-highlight ((t (:background "color-27"))))
+ '(match ((t (:background "color-28"))))
+ '(minibuffer-prompt ((t (:foreground "color-33"))))
+ '(mode-line ((t (:background "color-235" :foreground "color-245" :box (:line-width -1 :style released-button)))))
+ '(mode-line-buffer-id ((t (:foreground "color-87" :weight bold))))
+ '(mode-line-emphasis ((t (:foreground "color-22" :weight bold))))
+ '(mode-line-highlight ((t (:box (:line-width 2 :color "color-35" :style released-button)))))
+ '(mode-line-inactive ((t (:inherit mode-line :background "color-245" :foreground "color-235" :box (:line-width -1 :color "grey75") :weight light))))
+ '(region ((t (:background "color-236"))))
+ '(secondary-selection ((t (:background "color-23"))))
+ '(whitespace-line ((t (:background "gray20" :foreground "magenta" :underline t))))
+ '(whitespace-tab ((t (:background "color-232" :foreground "lightgray"))))
+ '(widget-field ((t (:background "color-220" :foreground "black")))))
